c649babb86fd5dfd7043de587e5fbdd8b212a991
TalissonMelo <talissonmelorodrigues@gmail.com>
2019-10-25 09:02:29 -0300

    Projeto criado

---
+++ src/main/java/com/talissonMelo/cursomdc/CursomdcApplication.java
@@ -0,0 +1,13 @@
+package com.talissonMelo.cursomdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomdcApplication.class, args);
+	}
+
+}
