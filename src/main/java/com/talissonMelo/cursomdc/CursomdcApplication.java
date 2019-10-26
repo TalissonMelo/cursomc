0674771870a74ad327db19d3877f9dc5127f4d18
TalissonMelo <talissonmelorodrigues@gmail.com>
2019-10-26 12:08:54 -0300

    Criando operação de instanciação

--- src/main/java/com/talissonMelo/cursomdc/CursomdcApplication.java
+++ src/main/java/com/talissonMelo/cursomdc/CursomdcApplication.java
@@ -1,13 +1,32 @@
 package com.talissonMelo.cursomdc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomdcApplication {
+import com.talissonMelo.cursomdc.domain.Categoria;
+import com.talissonMelo.cursomdc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomdcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomdcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
--- src/main/java/com/talissonMelo/cursomdc/resources/CategoriaResource.java
+++ src/main/java/com/talissonMelo/cursomdc/resources/CategoriaResource.java
@@ -1,5 +1,6 @@
 package com.talissonMelo.cursomdc.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +14,10 @@ import com.talissonMelo.cursomdc.services.CategoriaService;
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
+	@Autowired
 	private CategoriaService service;
 	
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	@RequestMapping(value = "/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
 		Categoria obj = service.buscar(id);
